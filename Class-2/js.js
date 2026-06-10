ea2c6d42b7f061f77ba6bce385d0ca90a3a71eeb
luvin25111-del <luvdaku2212@gmail.com>
2026-06-10 13:34:26 +0530

    Class-2

---
+++ Class-2/js.js
@@ -0,0 +1,6 @@
+//In hoisting the variable declaration and function declaration will move to top of the scope
+a = 10
+console.log(a);
+var a = 10
+//Hoisting only performs in var variable , in let and const hoisting perform ni ho rha
+
