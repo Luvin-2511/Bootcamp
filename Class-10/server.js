50ba7b290c30cff3e3ca5a89a39bfa24ed1308ae
luvin25111-del <luvdaku2212@gmail.com>
2026-06-23 12:14:24 +0530

    Updated

--- Class-10/server.js
+++ Class-10/server.js
@@ -1,10 +1,20 @@
 const express = require("express");
 
-const app = express()
+const app = express();
+
+app.get("/", (req,res) => {
+    res.send("This is home page")
+});
+app.get("/about", (req,res) => {
+    res.send("This is About page")
+});
+app.get("/contact", (req,res) => {
+    res.send("This is Contact page")
+});
 
 app.listen(3000, () => {
-    console.log(`Server listening at PORT:${3000}`);
-})
+  console.log(`Server listening at PORT:${3000}`);
+});
 
 // const { add, subtract } = require("./math");
 
