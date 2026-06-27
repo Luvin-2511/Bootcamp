784bb308f9b3f5cd1e3d809f339354860059da8a
luvin25111-del <luvdaku2212@gmail.com>
2026-06-27 20:12:35 +0530

    Complete authentication Task

---
+++ Class-13/src/app.js
@@ -0,0 +1,16 @@
+import 'dotenv/config'
+import express from "express";
+import authRouter from "./routes/auth.route.js";
+import cookieParser from "cookie-parser";
+
+const app = express();
+
+export const data = []
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+app.use("/api/auth", authRouter);
+
+export default app;
