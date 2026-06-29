b35ff64af812f4d89e2895f676aa50c33033de9b
luvin25111-del <luvdaku2212@gmail.com>
2026-06-29 15:04:02 +0530

    Frontend done

--- Class-13/Frontend/src/AppRoutes.jsx
+++ Class-13/Frontend/src/AppRoutes.jsx
@@ -1,13 +1,15 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes, useNavigate } from "react-router-dom";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Home from "./pages/Home";
 
 const AppRoutes = () => {
+  const nav = useNavigate()
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/login" element={<Login />} />
+        <Route path="/" element={nav('/login')} />
         <Route path="/register" element={<Register />} />
         <Route path="/home" element={<Home />} />
       </Routes>
